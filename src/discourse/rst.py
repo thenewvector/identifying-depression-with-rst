b427e9679ed607b69d6d4440a4e051a6655f1ceb
Evgeni M. <research@thenewvector.xyz>
2026-06-06 13:21:42 +0300

    upd: changed the init_pareser function to be able to use the unirst model

--- src/discourse/rst.py
+++ src/discourse/rst.py
@@ -16,10 +16,22 @@ def _require_parser() -> None:
 
 def init_parser(model: str = "tchewik/isanlp_rst_v3",
                 version: str = "rstreebank",
-                cuda: int = -1) -> None:
+                cuda: int = -1,
+                relinventory: str | None = None) -> None:
+    
     global _PARSER
     from isanlp_rst.parser import Parser
-    _PARSER = Parser(hf_model_name=model, hf_model_version=version, cuda_device=cuda)
+
+    kwargs = {
+        "hf_model_name": model,
+        "hf_model_version": version,
+        "cuda_device": cuda,
+    }
+    
+    if relinventory is not None:
+        kwargs["relinventory"] = relinventory
+
+    _PARSER = Parser(**kwargs)
 
 
 def _normalize_for_parser(s: str) -> str:
